b88862c6f46eca6d0382e34c036d8be88efd36c5
Toshifumi Kurosawa <kuromailserver@gmail.com>
2019-03-28 01:24:30 +0900

    Working With Templates

--- vidjot/app.js
+++ vidjot/app.js
@@ -1,19 +1,30 @@
-const express = require("express");
+const express = require('express');
+const exphbs  = require('express-handlebars');
 
 const app = express();
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+
 // Index Route
-app.get('/',(req,res) => {
-    res.send('INDEX')
+app.get('/', (req, res) => {
+  const title = 'Welcome';
+  res.render('index', {
+    title: title
+  });
 });
 
 // About Route
-app.get('/about',(req,res) => {
-    res.send('About')
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
 const port = 5000;
 
-app.listen(port, ()=>{
-    console.log(`Server started on port ${port}`);
+app.listen(port, () =>{
+  console.log(`Server started on port ${port}`);
 });
